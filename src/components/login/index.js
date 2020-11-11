9549c421ae70abe988ab6147d866cf85ee8a5e20
TMendesO <37598914+TMendesO@users.noreply.github.com>
2020-11-10 21:50:30 -0300

    criei os diretorios e login

---
+++ src/components/login/index.js
@@ -0,0 +1,13 @@
+import { FaSpotify } from 'react-icons/fa';
+
+function Login() {
+  return (
+    <>
+      <FaSpotify />
+
+      <button>Login</button>
+    </>
+  );
+}
+
+export default Login;
