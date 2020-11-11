3344fb96e8e1ef2532dc047d94d91b87c4275745
TMendesO <37598914+TMendesO@users.noreply.github.com>
2020-11-11 10:24:44 -0300

    integrando o login do spotify paraa usar o token

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,13 +1,35 @@
+import React from 'react';
 import { FaSpotify } from 'react-icons/fa';
 
-function Login() {
-  return (
-    <>
-      <FaSpotify />
-
-      <button>Login</button>
-    </>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    const parametros = this.getHashParams();
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+  render() {
+    return (
+      <>
+        <FaSpotify />
+        <div className="App">
+          <button>
+            <a href="http://localhost:8888">Login</a>
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Login;
